bc6bde66b9e06babbb876771f38fe7a0a6aee1c8
Yulia Miller <miller82.98@mail.ru>
2023-01-10 10:33:38 -0800

    Добавьте файлы проекта.

---
+++ 6.5.2. geometric shape/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+    }
+}
+class Triangle
+{
+    public int a;
+    public int b;
+    public int c;
+
+    public double Square()
+    {
+
+    }
+    public double Perimetr()
+    {
+
+    }
+}
+
+class Circle
+{
+    public double radius;
+
+    public double Square()
+    {
+
+    }
+    public double Length()
+    {
+
+    }
+}
+
+class Square
+{
+    public int side;
+
+    public double Square()
+    {
+
+    }
+    public double Perimetr()
+    {
+
+    }
+}
